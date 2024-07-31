6d319e375c54082564945ba6405f63b84de6acdb
Vineet Sawhney <vineet@persona3.io>
2024-07-31 23:35:35 +0530

    fix: minor styling adjusments for persona pay card

--- app/checkout/_components/checkout-form/_components/payment-methods.tsx
+++ app/checkout/_components/checkout-form/_components/payment-methods.tsx
@@ -27,19 +27,19 @@ const PaymentMethods = () => {
         <Image src={PaypalImg} alt="PayPal" className="h-6" />
       </div>
 
-      <div className="flex w-full flex-col items-center gap-4 rounded-b-lg border border-[#253B80] bg-[#EFF5FF] px-3 py-4 pb-6 font-normal">
-        <div className="mb-2 flex items-center self-start">
-          <input type="radio" id="card" name="paymentMethod" className="mr-2" checked />
-          <div className='flex gap-2 md:gap-1 items-center justify-center flex-wrap -ml-4 md:ml-0'>
+      <div className="flex w-full items-center rounded-b-lg border border-[#253B80] bg-[#EFF5FF] px-3 py-4 pb-6 font-normal">
+        <input type="radio" id="card" name="paymentMethod" className="mr-2 self-start" checked/>
+        <div className="mb-2 flex flex-col items-center self-start -ml-4 md:ml-0 gap-4 md:gap-4">
+          <div className="flex flex-col md:flex-row justify-center items-center w-full md:items-center md:justify-start gap-2 md:ml-0  md:gap-0">
             <Image src={PersonaImg} height={60} width={140} alt="Persona" className="h-4" />
             <span className="text-sm">Pay with Persona to claim offers</span>
           </div>
+          <Image src={RedirectIcon} alt="redirect-icon" className="h-16 w-28"></Image>
+          <p className="px-2 md:px-12 text-center text-sm text-gray-600">
+            After clicking Pay now, you will be redirected to Persona - Sponsored checkout to
+            complete your purchase securely.
+          </p>
         </div>
-        <Image src={RedirectIcon} alt="redirect-icon" className="h-16 w-28"></Image>
-        <p className="px-12 text-center text-sm text-gray-600">
-          After clicking Pay now, you will be redirected to Persona - Sponsored checkout to complete
-          your purchase securely.
-        </p>
       </div>
 
       <div className="mb-4 mt-4 flex items-center">
